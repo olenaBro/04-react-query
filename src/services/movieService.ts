c3a4a99f87ee43845a36a66598369fffe20cd18e
olenaBro <olenkazaria@gmail.com>
2026-04-09 15:28:59 +0200

    Initial commit

---
+++ src/services/movieService.ts
@@ -0,0 +1,19 @@
+import axios from "axios";
+import { Movie } from "../types/movie";
+
+interface MoviesResponse {
+  results: Movie[];
+}
+
+export const fetchMovies = async (query: string): Promise<Movie[]> => {
+  const response = await axios.get<MoviesResponse>(
+    "https://api.themoviedb.org/3/search/movie",
+    {
+      params: { query },
+      headers: {
+        Authorization: `Bearer ${import.meta.env.VITE_TMDB_TOKEN}`,
+      },
+    }
+  );
+  return response.data.results;
+};
